de091a14abe8e681e5d932cbbcf427098502a572
matthewnfaulkner <matthewnfaulkner@gmail.com>
2025-11-12 18:43:32 +0800

    Initial commit

---
+++ stylelint.config.js
@@ -0,0 +1,18 @@
+/** @type {import('stylelint').Config} */
+
+export default {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-rational-order',
+    'stylelint-config-recommended-vue',
+  ],
+  rules: {
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['tailwind', 'apply', 'variants', 'responsive', 'screen', 'layer'],
+      },
+    ],
+    'no-descending-specificity': null,
+  },
+};
