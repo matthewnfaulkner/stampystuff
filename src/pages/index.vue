63f312f325bf05a6c88758a5fb6fe980ece2a7a5
Matthew Faulkner <matthewnfaulkner@gmail.com>
2025-11-13 10:40:07 +0800

    added stroke as an option for colors

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -7,10 +7,26 @@ const { currentTheme, switchTheme } = useTheme();
 
 const primaryColor = ref(getComputedStyle(document.documentElement).getPropertyValue('--color-primary').trim() || '#007ad9');
 const textColor = ref(getComputedStyle(document.documentElement).getPropertyValue('--color-text').trim() || '#000000');
+const strokeColor = ref(getComputedStyle(document.documentElement).getPropertyValue('--color-stroke').trim() || '#000000');
+let strokeToggle = false;
 
 const updatePrimaryColor = () => {
   const value = primaryColor.value.startsWith('#') ? primaryColor.value : `#${primaryColor.value}`;
   document.documentElement.style.setProperty('--color-primary', value);
+};
+
+const updateStrokeColor = () => {
+  const value = strokeColor.value.startsWith('#') ? strokeColor.value : `#${strokeColor.value}`;
+  document.documentElement.style.setProperty('--color-stroke', value);
+};
+
+const updateStrokeToggle = () => {
+  if (!strokeToggle) {
+    document.documentElement.style.setProperty('--stroke-width', '3px');
+  } else {
+    document.documentElement.style.setProperty('--stroke-width', '0px');
+  }
+  strokeToggle = !strokeToggle;
 };
 
 const updateTextColor = () => {
@@ -27,49 +43,7 @@ const updateTextColor = () => {
 
 <template>
   <header class="absolute inset-x-0 top-0">
-    <div class="container mx-auto flex justify-end p-4">
-      <button
-        class="overflow-hidden p-2"
-        @click="switchTheme()"
-      >
-        <transition
-          enter-active-class="transition duration-200 ease-out"
-          leave-active-class="transition duration-200 ease-in"
-          :enter-from-class="currentTheme === 'dark' ? 'transform -translate-y-full scale-50 opacity-0' : 'transform translate-y-full scale-50 opacity-0'"
-          enter-to-class="transform translate-y-0"
-          leave-from-class="transform translate-y-0"
-          :leave-to-class="currentTheme === 'dark' ? 'transform translate-y-full scale-50 opacity-0' : 'transform -translate-y-full scale-50 opacity-0'"
-          mode="out-in"
-        >
-          <svg
-            v-if="currentTheme === 'dark'"
-            xmlns="http://www.w3.org/2000/svg"
-            class="size-8"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-          >
-            <path d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
-          </svg>
-
-          <svg
-            v-else
-            xmlns="http://www.w3.org/2000/svg"
-            class="size-8"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-          >
-            <path d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
-          </svg>
-        </transition>
-      </button>
+    <div class="container mx-auto flex justify-between p-1">
       <div class="color-picker-container">
         <h3>Primary Color</h3>
         <ColorPicker
@@ -82,12 +56,20 @@ const updateTextColor = () => {
           v-model="textColor"
           @change="updateTextColor"
         />
+
+        <h3>Stroke Color</h3>
+        <ColorPicker
+          v-model="strokeColor"
+          @change="updateStrokeColor"
+        />
+        <input type="checkbox" id="myCheckbox" v-model="isChecked" @change="updateStrokeToggle">
+        <label for="myCheckbox">Toggle Stroke</label>
       </div>
     </div>
   </header>
 
   <main class="mx-auto flex h-screen max-w-3xl flex-col items-center justify-center px-4 py-16">
-    <h1 class="dark:text-primary text-primary pb-20 text-center text-6xl transition-colors ">
+    <h1 class="dark:text-primary text-primary text-stroke pb-20 text-center text-6xl transition-colors ">
       Stampy Stuff
     </h1>
     <h1 class="dark:text-primary text-primary pb-20 text-center text-4xl transition-colors ">
@@ -95,6 +77,49 @@ const updateTextColor = () => {
     </h1>
 
     <div class="flex justify-center space-x-4 pb-10" />
+    <button
+      class="overflow-hidden p-2"
+      style="display: none;"
+      @click="switchTheme()"
+    >
+      <transition
+        enter-active-class="transition duration-200 ease-out"
+        leave-active-class="transition duration-200 ease-in"
+        :enter-from-class="currentTheme === 'dark' ? 'transform -translate-y-full scale-50 opacity-0' : 'transform translate-y-full scale-50 opacity-0'"
+        enter-to-class="transform translate-y-0"
+        leave-from-class="transform translate-y-0"
+        :leave-to-class="currentTheme === 'dark' ? 'transform translate-y-full scale-50 opacity-0' : 'transform -translate-y-full scale-50 opacity-0'"
+        mode="out-in"
+      >
+        <svg
+          v-if="currentTheme === 'dark'"
+          xmlns="http://www.w3.org/2000/svg"
+          class="size-8"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+        >
+          <path d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+        </svg>
+
+        <svg
+          v-else
+          xmlns="http://www.w3.org/2000/svg"
+          class="size-8"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+        >
+          <path d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
+        </svg>
+      </transition>
+    </button>
   </main>
 </template>
 
@@ -102,10 +127,10 @@ const updateTextColor = () => {
 
 .color-picker-container {
   display: flex;
+  flex-direction: row !important;
   flex-direction: column;
   gap: 1rem;
-  max-width: 250px;
-  margin: 2rem;
+  margin: .5rem;
 }
 
 .p-colorpicker-preview{
